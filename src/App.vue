3e68e79bc0804e3ff9a9c84d964a429fadbe2117
Paweł Życiński <zoritt@gmail.com>
2019-11-24 18:44:10 +0100

    Rozdzielenie atrybutów obiektu vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
+<template src = "./App.html"/>  
 <script>
 export default {
-  template: `<div class="text-center p-3">
-<h1 class="bg-secondary text-white p-3">
-{{ message }}
-</h1>
-<button class="btn btn-secondary" v-on:click="handleClick">
-Wciśnij mnie
-</button>
-</div>`,
   data: function() {
     return {
       counter: 0
